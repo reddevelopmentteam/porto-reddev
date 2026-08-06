ee150d5ba3f961a0c8abc25313acc12a68dc70ef
ghaizan17 <ghaizan.pratama.maulana@gmail.com>
2026-08-06 20:31:29 +0800

    fix bug role

--- database/migrations/2026_08_01_112612_create_roles_table.php
+++ database/migrations/2026_08_01_112612_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->json('name');
+            $table->string('name');
             $table->softDeletes();
             $table->timestamps();
         });
