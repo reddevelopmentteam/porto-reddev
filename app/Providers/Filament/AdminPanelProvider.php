9b43614a1d29978617e40b5e696351aef3091f61
Ghaizan17 <ghaizan.pratama.maulana@gmail.com>
2026-08-12 22:17:10 +0800

    Feature/setting (#47)
    * Membuat custom page untuk setting dan memperbaiki bug pada team members table
    
    * FINAL BACKENDDD

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,6 +11,7 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Enums\ThemeMode;
 use Filament\View\PanelsRenderHook;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -33,13 +34,15 @@ class AdminPanelProvider extends PanelProvider
             ->brandLogoHeight('4rem')
             ->renderHook(
                 PanelsRenderHook::STYLES_AFTER,
-                fn (): string => view('filament.hooks.logo-styles')->render(),
+                fn (): string => view('filament.hooks.app-styles')->render(),
             )
             ->spa()
             ->login()
             ->colors([
                 'primary' => Color::Red,
             ])
+            ->darkMode()
+            ->defaultThemeMode(ThemeMode::Dark)
             ->sidebarWidth('15rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
