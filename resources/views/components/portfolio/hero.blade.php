620ee9c49ef309bad47d2d77d92609647f36fadc
Ghaizan17 <ghaizan.pratama.maulana@gmail.com>
2026-08-15 09:46:33 +0800

    Membuat hero section

--- resources/views/components/portfolio/hero.blade.php
+++ resources/views/components/portfolio/hero.blade.php
@@ -1,3 +1,56 @@
-<div>
+<section class="center-layout h-[48rem]">
+    <div class="mt-10 flex flex-col gap-5" >
+        <!-- text hero -->
+        <div class="text-center font-display font-bold text-6xl flex flex-col space-y-5" >
+            <h1>We Build</h1>
+            <div class="flex justify-center" >
+                <h1 class="bg-white text-black">Digital Product</h1>
+            </div>
+            <h1>that move businesses forward<span class="text-primary" >.</span></h1>
+            <div class="flex justify-center font-inter">
+                <p class="font-normal text-sm w-[60%]" >We design and develop websites, digital products, and software that are built to perform and grow.</p>
+            </div>
+        </div>
+        <!-- feature -->
+        <div class="center-layout mt-10">
+            <section class="grid grid-cols-3 w-[70%] gap-10 ">
+                @php
+                    $features = [
+                        [
+                            'icon' => 'material-symbols:code-xml-rounded',
+                            'title' => 'Clean Code',
+                            'description' => 'Scalable, maintainable, and future-ready.',
+                        ],
+                        [
+                            'icon' => 'material-symbols:check-circle-outline-rounded',
+                            'title' => 'Quality Focused',
+                            'description' => 'Pixel-perfect, tested, and reliable.',
+                        ],
+                        [
+                            'icon' => 'material-symbols:show-chart-outline-rounded',
+                            'title' => 'Business Driven',
+                            'description' => 'Solutions that solve problems and drive growth.',
+                        ],
+                    ];
+                @endphp
 
-</div>
+                @foreach ($features as $feature)
+                    <div class="pr-5 border-r border-gray-500/40 last:border-0">
+                        <div class="mb-1 text-4xl text-primary">
+                            {{-- icon --}}
+                            <iconify-icon icon="{{$feature['icon']}}" />
+                        </div>
+
+                        <h3 class="text-lg font-semibold text-white">
+                            {{ $feature['title'] }}
+                        </h3>
+
+                        <p class="mt-2 text-sm w-48 text-gray-300">
+                            {{ $feature['description'] }}
+                        </p>
+                    </div>
+                @endforeach
+            </section>
+        </div>
+    </div>
+</section>
--- resources/views/components/portfolio/navbar.blade.php
+++ resources/views/components/portfolio/navbar.blade.php
@@ -1,22 +1,37 @@
-<nav class="w-full relative">
+<nav class="w-full max-w-[90rem] relative"
+    x-data="{ active: window.location.hash || '#home' }"
+    x-init="
+        window.addEventListener('hashchange', () => {
+            active = window.location.hash || '#home'
+    }"
+>
     <!-- left nav -->
-    <section class="p-4 rounded-l-4xl max-w-7xl right-diagonal bg-glass/40">
+    <section class="p-2 rounded-l-4xl max-w-[79rem] right-diagonal navbar-glass bg-glass/40">
         <!-- logo -->
-        <div class="absolute top-3 left-8" >
-            <img src="{{ asset('images/logo.svg') }}" alt="">
+        <div class="absolute top-4 left-8" >
+            <a href="#" wire:navigate >
+                <img src="{{ asset('images/logo.svg') }}" alt="">
+            </a>
         </div>
         <!-- nav menu -->
-        <div class="center-layout gap-7 font-semibold                                                                                                                           " >
-            <a href="">Work</a>
-            <a href="">Services</a>
-            <a href="">About</a>
-            <a href="">Process</a>
+        <div class="center-layout gap-8 font-semibold">
+            @foreach (['work', 'services', 'about', 'process'] as $menu)
+                <a href="#{{ $menu }}" wire:navigatez
+                    class="rounded-xl px-4 py-2 active:scale-95 font-inter capitalize"
+                    :class="active === '#{{ $menu }}'
+                    ? 'bg-gray-800/50'
+                    : 'hover:bg-gray-800/50'"
+                    >
+                    {{ $menu }}
+                </a>
+                
+            @endforeach
         </div>
     </section>
     <!-- right nav -->
     <section class="absolute right-3 top-0 rounded-r-4xl w-52 left-diagonal bg-primary p-4">
         <div class="center-layout">
-            <p class="text-white font-bold">Let’s Talk</p>
+            <p class="text-white font-bold font-inter">Let’s Talk</p>
         </div>
     </section>
 </nav>
